19af715e0b01c10d34ae252d88149372c59ac3c1
HB767 <173813415@qq.com>
2018-03-26 20:34:29 +0800

    kk

--- ssm-redis/src/main/java/com/ht/controller/HelloController.java
+++
@@ -1,6 +0,0 @@
-package com.ht.controller;
-public class HelloController{
-	public void test(){
-		System.out.println("kkkk");
-	}
-}
--- ssm-redis/src/main/java/com/ht/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.ht.dao;
-
-import com.ht.bean.User;
-import com.ht.common.BaseDAO;
-
-
-public interface UserDao extends BaseDAO<User> {
-
-}
